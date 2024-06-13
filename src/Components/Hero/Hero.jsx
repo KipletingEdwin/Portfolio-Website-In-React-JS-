17c4f1354aba9ba21fd11e0da88bd1849b7681f0
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-13 15:34:13 +0300

    add contents of hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Hero.css";
+import profileImg from "../../assets/assets/profile_img.svg";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <img src={profileImg} alt="profile" />
+      <h1><span>I'm Alex Bennett,</span> frontend developer based in USA.</h1>
+      <p>
+        I am a frontend developer from Califonia, USA with 10 years of
+        experience in multiple companies like Microsoft, Tesla and Apple.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;

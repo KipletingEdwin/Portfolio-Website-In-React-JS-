31733bc513ce73dee48115255cf0b2712bb68949
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-13 14:47:25 +0300

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ export const App = () => {
   return (
     <div>
       <Navbar />
-
-      
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,12 +7,13 @@ export const Navbar = () => {
     <div className='navbar'>
         <img src={logo} alt='logo' />
         <ul className="nav-menu">
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
+            <li> Home </li>
+            <li> About Me </li>
+            <li> Services </li>
+            <li> Portfolio </li>
+            <li> Contact </li>
         </ul>
+        <div className="nav-connect">Connect With Me</div>
 
     </div>
   )

0921f39794e6a9d8786b379829c110ef3f424638
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-13 14:12:22 +0300

    add navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Navbar } from './Components/Navbar/Navbar'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+
+      
+    </div>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/assets/logo.svg'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={logo} alt='logo' />
+        <ul className="nav-menu">
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+        </ul>
+
+    </div>
+  )
+}

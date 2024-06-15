9630622d062e968006788eff5f8cc12a1af1bacb
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-15 09:27:49 +0300

    add services data

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Services.css'
 import theme_pattern from '../../assets/assets/theme_pattern.svg'
+import servicesData from '../../Data/services.json'
+import arrow_icon from '../../assets/assets/arrow_icon.svg'
+
 
 export const Services = () => {
   return (
@@ -8,6 +11,23 @@ export const Services = () => {
         <div className="services-title">
             <h1>My Services</h1>
             <img src={theme_pattern} alt='services' />
+        </div>
+        <div className="services-container">
+          {
+            servicesData.map((service, index)=>{
+              return <div key={index} className="services-format">
+                <h3>{service.s_no}</h3>
+                <h2>{service.s_name}</h2>
+                <p>{service.s_desc}</p>
+                <div className="services-readmore">
+                  <p>Read More</p>
+                  <img src={arrow_icon} alt='arrow-icon' />
+                </div>
+
+              </div>
+            })
+          }
+
         </div>
     </div>
   )

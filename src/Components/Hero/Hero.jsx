2d7a5451e027027ef3ee18e6740e1adaf6c80a46
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-03 16:01:14 +0300

    add anchor-link tag

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import profileImg from "../../assets/assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
@@ -12,7 +13,7 @@ const Hero = () => {
         experience in multiple companies like Microsoft, Tesla and Apple.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect"> <AnchorLink className="anchor-link" offset={50} href="#contact"> Connect with me   </AnchorLink>   </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
